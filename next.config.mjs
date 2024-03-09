773714640dc4a8e6347e40836276098208b89f78
maxvyr <60290207+Maxvyr@users.noreply.github.com>
2024-03-09 11:22:12 +0100

    add PWA system to this web app

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
+import nextPWA from "next-pwa"
+
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   experimental: {
     appDir: true,
   },
 }
 
-export default nextConfig
+const withPWA = nextPWA({
+  dest: "public", // Destination directory for the PWA files
+  disable: process.env.NODE_ENV === "development", // Disable PWA in development mode
+  register: true, // Register the PWA service worker
+  skipWaiting: true, // Skip waiting for service worker activation
+})
+
+export default withPWA(nextConfig)
